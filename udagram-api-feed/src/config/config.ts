ec5c551d37dc6db68336f868618c51607819c594
VnFood <tranloi.tphl@gmail.com>
2024-06-13 18:53:49 +0700

    Update

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,16 +1,4 @@
 export const config = {
-  // username: 'loitph',
-  // password: 'loitph2024',
-  // database: 'postgres',
-  // host: 'cdr.cotxhyhgbj58.us-east-1.rds.amazonaws.com',
-  // dialect: 'postgres',
-  // aws_region: 'us-east-1',
-  // aws_profile: 'default',
-  // aws_media_bucket: 'arn:aws:s3:::s3proj3refactor',
-  // url: 'http://localhost:8100',
-  // jwt: {
-  //   secret: 'hellojwt',
-  // },
   'username': process.env.POSTGRES_USERNAME,
   'password': process.env.POSTGRES_PASSWORD,
   'database': process.env.POSTGRES_DB,
